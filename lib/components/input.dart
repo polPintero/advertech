2374bca3071d4aec299b3d1ab5077d6c0159b205
Dmitriy <johndoe@example.com>
2023-06-29 12:31:01 +0300

    added form to input

--- lib/components/input.dart
+++ lib/components/input.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Input extends StatelessWidget {
-  Input({
-    this.label = '',
-    this.type = '',
-  });
+  Input({this.label = '', this.type = '', this.onChanged, this.formKey});
   final String label;
   final String type;
+  final void Function(String)? onChanged;
+  final GlobalKey? formKey;
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      style: TextStyle(fontSize: 24.0),
-      decoration: InputDecoration(
-        labelText: label,
-        icon: Icon(Icons.lock_open),
+    return Form(
+      key: formKey,
+      child: TextFormField(
+        style: TextStyle(fontSize: 24.0),
+        decoration: InputDecoration(
+          labelText: label,
+          icon: Icon(Icons.lock_open),
+        ),
+        onChanged: onChanged,
+        validator: (String? value) {
+          dynamic validationResult = null;
+          final processValue = value?.trim();
+          if (processValue == null || processValue.isEmpty) {
+            validationResult = 'This field is required';
+          }
+          return validationResult;
+        },
       ),
-      validator: (String? value) {
-        dynamic validationResult = null;
-        final processValue = value?.trim();
-        if (processValue == null || processValue.isEmpty) {
-          validationResult = 'This field is required';
-        }
-
-        return validationResult;
-
-
-      },
     );
   }
 }
